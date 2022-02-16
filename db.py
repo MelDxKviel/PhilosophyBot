82a89a5e162d75381733aa52e0dd56934049c802
MelDxKviel <mega1hack52@gmail.com>
2022-02-16 19:51:27 +0700

    ADD: get philosopher from db

--- db.py
+++ db.py
@@ -1,3 +1,14 @@
 import sqlite3
 
-# работа с базой данных
+conn = sqlite3.connect("db.sqlite3")
+cursor = conn.cursor()
+
+
+def get_philosopher(philosopher: str):
+    cursor.execute(f"SELECT bio FROM philosophers "
+                   f"WHERE name LIKE '%{philosopher}%'")
+    try:
+        result = cursor.fetchone()[0]
+    except TypeError:
+        return None
+    return result
