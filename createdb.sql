2005176a796f9b86a28eddef660bd562910d7aef
MelDxKviel <mega1hack52@gmail.com>
2022-02-16 22:46:02 +0700

    UPD: make id autoincrement

--- createdb.sql
+++ createdb.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS philosophers
 (
-    philosopher_id integer NOT NULL PRIMARY KEY,
+    philosopher_id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(64) NOT NULL,
     bio TEXT NOT NULL
 )
