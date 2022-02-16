3c6a25d3b66f4382f8b59160bbb773c2da912e45
MelDxKviel <mega1hack52@gmail.com>
2022-02-16 22:46:21 +0700

    ADD: quotes table

--- createdb.sql
+++ createdb.sql
@@ -3,4 +3,12 @@ CREATE TABLE IF NOT EXISTS philosophers
     philosopher_id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(64) NOT NULL,
     bio TEXT NOT NULL
-)
+);
+
+CREATE TABLE IF NOT EXISTS quotes
+(
+   quote_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+   quote_test TEXT NOT NULL,
+   philosopher_id INTEGER NOT NULL,
+   FOREIGN KEY (philosopher_id) REFERENCES philosophers(philosopher_id)
+);
